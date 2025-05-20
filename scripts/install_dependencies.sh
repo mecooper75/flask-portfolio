6c130ce4c28cda729196b7e20bfd2d709b53bfcf
mecooper75 <lilcoop55@yahoo.com>
2025-05-20 20:04:20 +0000

    Add logging and error checking to install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+set -e  # Exit on first error
+
 cd /home/ec2-user/flask-portfolio
+
+# Ensure Python 3 is available
+which python3
+
+# Set up and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+
+# Install requirements and log output
+pip install --upgrade pip
+pip install -r requirements.txt | tee /tmp/pip-install.log
+
